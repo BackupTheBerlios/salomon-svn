2522dbbf609c15daf9afd35f670209feadd399a3
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-09 16:05:14 +0000

    First implementation.

--- src/ks/Starter.java
+++ src/ks/Starter.java
@@ -5,22 +5,54 @@
 
 package ks;
 
-import ks.plugins.Plugin;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import ks.core.IManager;
+import ks.core.ManagerEngine;
+import ks.core.StandAloneManager;
+import ks.data.DBConnector;
 
 /**
  *  
  */
-public class Starter
+public final class Starter
 {
-	///////////////////////////////////////
-	// associations
-	/**
-	 *  
-	 */
-	public Plugin plugin;
-	
+	private IManager _manager;
+
+	private ManagerEngine _managerEngine;
+
+	private DBConnector _dbConnector;
+
+	private static Logger _logger = Logger.getLogger(Starter.class);
+
+	public Starter()
+	{
+		PropertyConfigurator.configure("log.conf");
+		_logger.info("###  Application started  ###");
+	}
+
+	private void initManagers()
+	{
+		_manager = new StandAloneManager();
+		_managerEngine = new ManagerEngine();
+		try {
+			_dbConnector = DBConnector.getInstance();
+		} catch (SQLException e) {
+			_logger.fatal("", e);
+		} catch (ClassNotFoundException e) {
+			_logger.fatal("", e);
+		}
+	}
+
+	public void start()
+	{
+		initManagers();
+		_manager.start(_managerEngine);
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("Knowledge System started...");
+		(new Starter()).start();
 	}
 } // end Starter
