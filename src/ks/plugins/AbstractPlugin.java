d9842dee48d4cffd22c27357df09641f16d83db5
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-09 16:09:09 +0000

    Class representing abstact Plugin. Implements clone() method makes descendents implement IDataPlugin, IGraphicalPlugin interfaces.

---
+++ src/ks/plugins/AbstractPlugin.java
@@ -0,0 +1,46 @@
+/** Java interface "AbstractPlugin.java" generated from Poseidon for UML.
+ *  Poseidon for UML is developed by <A HREF="http://www.gentleware.com">Gentleware</A>.
+ *  Generated with <A HREF="http://jakarta.apache.org/velocity/">velocity</A> template engine.
+ */
+
+package ks.plugins;
+
+/**
+ *  
+ */
+public abstract class AbstractPlugin implements IDataPlugin, IGraphicalPlugin, Cloneable
+{
+	///////////////////////////////////////
+	// associations
+	///////////////////////////////////////
+	// operations
+	/**
+	 * Does ...
+	 *  
+	 */
+	public abstract void initizalize();
+
+	/**
+	 * Does ...
+	 *  
+	 */
+	public abstract void destroy();
+
+	/**
+	 * Does ...
+	 * 
+	 * @return
+	 */
+	public abstract Description getDescription();
+	
+    public Object clone(){
+        Object clone = null;
+        try {
+            clone = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return clone;
+    } 
+	
+} // end Plugin
