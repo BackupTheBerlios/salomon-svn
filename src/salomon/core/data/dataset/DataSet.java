86846b26404b730265feabc2f2e2e2f92c338cde
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-23 14:03:29 +0000

    Support for user SQL queries.

--- src/salomon/core/data/dataset/DataSet.java
+++ src/salomon/core/data/dataset/DataSet.java
@@ -10,12 +10,10 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
 import salomon.core.data.DBManager;
 import salomon.core.data.common.DBColumnName;
 import salomon.core.data.common.DBCondition;
 import salomon.core.data.common.DBTableName;
-
 import org.apache.log4j.Logger;
 
 /**
@@ -23,41 +21,102 @@ import org.apache.log4j.Logger;
  */
 public class DataSet
 {
-	private int _datasetId = 0;
-	
-	private DBCondition[] _conditions = null;	
+	/** Conditions determinating data set.
+	 * If _conditions == null it means that data set includes all data */
+	private DBCondition[] _conditions = null;
+
 	private DBTableName[] _tableNames = null;
-	
+
 	private static Logger _logger = Logger.getLogger(DataSet.class);
-	/**
-	 * @param dataSetId (niepotrzebne chyba)
-	 */
-	public DataSet(int dataSetId)
-	{
-		_datasetId = dataSetId;
-	}	
-	
+
 	public ResultSet selectData(DBColumnName[] columnNames,
-			DBTableName[] tableNames, DBCondition[] conditions) throws SQLException, ClassNotFoundException
+			DBTableName[] tableNames, DBCondition[] conditions)
+			throws SQLException, ClassNotFoundException
 	{
-		//preparing conditions
-		List conditonList = getConditionsByTable(tableNames);
 		DBCondition[] queryConditions = null;
-		_logger.info("DataSet.selectData(): conditonList = " + conditonList);
-		
-		if (conditonList.size() > 0) {			
-			//adding conditions determinating data set
-			queryConditions = new DBCondition[conditonList.size() + conditions.length];
-			conditonList.addAll(Arrays.asList(conditions));
-			queryConditions = (DBCondition[]) conditonList.toArray(queryConditions);			
+		if (_conditions != null) {
+			//
+			//preparing conditions
+			//
+			List conditonList = getConditionsByTable(tableNames);
+			_logger
+					.info("DataSet.selectData(): conditonList = "
+							+ conditonList);
+			if (conditonList.size() > 0) {
+				//
+				//adding conditions determinating data set
+				//
+				queryConditions = new DBCondition[conditonList.size()
+						+ conditions.length];
+				conditonList.addAll(Arrays.asList(conditions));
+				queryConditions = (DBCondition[]) conditonList
+						.toArray(queryConditions);
+			}
 		} else {
 			queryConditions = conditions;
-		}	 
-		
+		}
 		DBManager connector = DBManager.getInstance();
-		 
-		return connector.selectData(columnNames, tableNames, queryConditions);		 
+		return connector.selectData(columnNames, tableNames, queryConditions);
 	}
+
+	/**
+	 * Method returns ResultSet basing on given query. Query is modified - all
+	 * conditions determinating data set are concatenated to the query; TODO:
+	 * first implementation, improve it(add support for "order by" itp...)
+	 * 
+	 * @param query
+	 * @return @throws
+	 *         ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public ResultSet selectData(String query) throws SQLException,
+			ClassNotFoundException
+	{
+		_logger.info("given query: " + query);
+		String finalQuery = query.trim().toLowerCase();
+		//
+		// If there are some conditions determinating data set
+		// they are added to query
+		//
+		String dataSetCond = null;
+		if (_conditions != null) {
+			for (int i = 0; i < _conditions.length; i++) {
+				// to first condition "and" is not added
+				// it will be added later, if necessary
+				if (i > 0) {
+					dataSetCond += " and ";
+				}
+				dataSetCond += _conditions[i].toString();
+			}
+		}
+		_logger.debug("dataSetCond" + dataSetCond);
+		//
+		// If there are any conditions, given query has to be modified
+		//
+		if (dataSetCond != null) {
+			//
+			// removing semicolon if exists
+			//
+			if (finalQuery.endsWith(";")) {
+				finalQuery = finalQuery.substring(0, finalQuery.length() - 1);
+			}
+			//
+			// if there is where clause,
+			// then adding conditions at the end of query
+			// else adding where and conditions list
+			//			
+			if (finalQuery.indexOf("where") != -1) {
+				finalQuery += " where ";
+			} else {
+				finalQuery += " and " + dataSetCond;
+			}
+		}
+		_logger.info("finalQuery: " + finalQuery);
+		DBManager connector = DBManager.getInstance();
+		connector.executeQuery(finalQuery);
+		return connector.getResultSet();
+	}
+
 	/**
 	 * @return Returns the _conditions.
 	 */
@@ -65,15 +124,18 @@ public class DataSet
 	{
 		return _conditions;
 	}
-	/** Returns conditions which refers to tables specified by tableNames
-	 * Data set is a subset of data stored in tables, conditions specifies
-	 * how to get this subset. Conditions got from plugin has to be added 
-	 * to condition list determinating data set.   
+
+	/**
+	 * Returns conditions which refers to tables specified by tableNames Data
+	 * set is a subset of data stored in tables, conditions specifies how to get
+	 * this subset. Conditions got from plugin has to be added to condition list
+	 * determinating data set.
 	 * 
 	 * @param tableNames
 	 * @return
 	 */
-	private List getConditionsByTable(DBTableName[] tableNames) {
+	private List getConditionsByTable(DBTableName[] tableNames)
+	{
 		List conditionList = new LinkedList();
 		// Checking which condition determinating data set
 		// should be added to specified condition list
@@ -88,13 +150,15 @@ public class DataSet
 		}
 		return conditionList;
 	}
-	
+
 	public void setConditions(DBCondition[] conditions)
 	{
-		_conditions = conditions;		
+		_conditions = conditions;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString()
