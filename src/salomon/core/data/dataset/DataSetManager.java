613df5a700d6bb1969261d1f5e97507791557a44
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-23 14:04:19 +0000

    Support for data set which represents all data.

--- src/salomon/core/data/dataset/DataSetManager.java
+++ src/salomon/core/data/dataset/DataSetManager.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
-
 import salomon.core.data.DBManager;
 import salomon.core.data.common.DBColumnName;
 import salomon.core.data.common.DBCondition;
@@ -21,39 +20,53 @@ import salomon.core.data.common.DBTableName;
 public class DataSetManager
 {
 	private final static String DATASET_TABLE = "datasets";
+
 	private final static String DATASET_ITEMS = "dataset_items";
-	
-	public DataSet getDataSetForName(String name) throws SQLException, ClassNotFoundException {
+
+	public DataSet getDataSetForName(String name) throws SQLException,
+			ClassNotFoundException
+	{
 		DataSet dataSet = null;
 		ResultSet resultSet = getDataSetItems(name);
 		List conditions = new LinkedList();
-		
+		//
 		//getting conditions
+		//
 		while (resultSet.next()) {
 			String tableName = resultSet.getString("table_name");
 			String condition = resultSet.getString("condition");
 			conditions.add(new DBCondition(tableName, condition));
 		}
-		DBCondition[] conditionsArray = new DBCondition[conditions.size()];
-		dataSet = new DataSet(0);
-		dataSet.setConditions((DBCondition[]) conditions.toArray(conditionsArray));
+		dataSet = new DataSet();
+		DBCondition[] conditionsArray = null;
+		//
+		// if there is no conditions, data set includes all data
+		// and its conditions list remains null
+		//
+		if (!conditions.isEmpty()) {
+			conditionsArray = new DBCondition[conditions.size()];
+			dataSet.setConditions((DBCondition[]) conditions
+					.toArray(conditionsArray));
+		}
 		return dataSet;
 	}
-	
-	private ResultSet getDataSetItems(String dataSetName) throws SQLException, ClassNotFoundException {
+
+	private ResultSet getDataSetItems(String dataSetName) throws SQLException,
+			ClassNotFoundException
+	{
 		DBTableName[] tableNames = {new DBTableName("datasets"),
-				new DBTableName("dataset_items")				
-		};
-		DBColumnName[] columnNames = {new DBColumnName(tableNames[1], "table_name"),				
-									new DBColumnName(tableNames[1], "condition")
-					};
-		DBCondition[] conditions = {				
-				new DBCondition(new DBColumnName(tableNames[0], "dataset_id"), DBCondition.REL_EQ, new DBColumnName(tableNames[1], "dataset_id"), DBCondition.JOIN),
-				new DBCondition(new DBColumnName(tableNames[0], "dataset_name"), DBCondition.REL_EQ, dataSetName, DBCondition.TEXT)
-				};
-		
+				new DBTableName("dataset_items")};
+		DBColumnName[] columnNames = {
+				new DBColumnName(tableNames[1], "table_name"),
+				new DBColumnName(tableNames[1], "condition")};
+		DBCondition[] conditions = {
+				new DBCondition(new DBColumnName(tableNames[0], "dataset_id"),
+						DBCondition.REL_EQ, new DBColumnName(tableNames[1],
+								"dataset_id"), DBCondition.JOIN),
+				new DBCondition(
+						new DBColumnName(tableNames[0], "dataset_name"),
+						DBCondition.REL_EQ, dataSetName, DBCondition.TEXT)};
 		DBManager connector = DBManager.getInstance();
-		 
 		return connector.selectData(columnNames, tableNames, conditions);
 	}
 }
