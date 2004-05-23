2f5dcfa8f5eada9556cf21c8f2af01f35394ee35
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-23 14:10:26 +0000

    Support for loading tasks from external folder added.

--- src/salomon/controller/gui/TaskEditionManager.java
+++ src/salomon/controller/gui/TaskEditionManager.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,10 +19,12 @@ import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Logger;
 
-import pl.edu.agh.icsr.salomon.plugin.averageprice.AveragePrice;
+import salomon.core.plugin.PluginLoader;
+import salomon.core.plugin.PluginManager;
 import salomon.core.task.Task;
 import salomon.plugin.IPlugin;
 import salomon.plugin.IResultComponent;
@@ -34,23 +37,24 @@ import salomon.plugin.ISettings;
  */
 class TaskEditionManager
 {
+
+	private static Logger _logger = Logger.getLogger(TaskEditionManager.class);
+
 	private JList _pluginList = null;
 
 	private DefaultListModel _pluginListModel = null;
+
+	private JPopupMenu _pluginPopup = null;
+
+	private MouseListener _popupListener = null;
+
+	private int _selectedItem = -1;
 
 	private JList _taskList = null;
 
 	private DefaultListModel _taskListModel = null;
 
 	private JPopupMenu _taskPopup = null;
-
-	private JPopupMenu _pluginPopup = null;
-
-	private int _selectedItem = -1;
-
-	private MouseListener _popupListener = null;
-
-	private static Logger _logger = Logger.getLogger(TaskEditionManager.class);
 
 	public TaskEditionManager()
 	{
@@ -62,29 +66,45 @@ class TaskEditionManager
 		_popupListener = new PopupListener();
 		_taskList.addMouseListener(_popupListener);
 		_pluginList.addMouseListener(_popupListener);
-		// sample - TODO: add here PluginLoadera
-		_pluginListModel.addElement(new AveragePrice());
-		//_pluginListModel.addElement(new SimpleSQLConsole());
+
+		//TODO: getting plugin from Engine?
+		File[] files = new PluginManager().getAvailablePlugins();
+
+		for (int i = 0; i < files.length; i++) {
+			_pluginListModel.addElement(new LocalPlugin(files[i]));
+		}
 	}
 
 	/**
-	 * Adds selected plugin to list of tasks and removes it from available
-	 * plugin list (?)
-	 *  
+	 * Adds selected plugin to list of tasks
 	 */
 	public void addTask()
 	{
 		int index = _pluginList.getSelectedIndex();
 		if (index >= 0) {
-			IPlugin plugin = (IPlugin) _pluginListModel.get(index);
-			Task task = new Task();
-			task.setPlugin(plugin);
-			_taskListModel.addElement(task);
+			LocalPlugin localPlugin = (LocalPlugin) _pluginListModel.get(index);
+			IPlugin plugin;
+			try {
+				plugin = localPlugin.getPlugin();
+				Task task = new Task();
+				task.setPlugin(plugin);			
+				task.setName(getTaskName());
+				_taskListModel.addElement(task);
+			} catch (Exception e) {
+				_logger.fatal("", e); //$NON-NLS-1$
+				showErrorMessage(Messages.getString("ERR_CANNOT_LOAD_PLUGIN"));	 //$NON-NLS-1$
+			}
 		} else {
-			_logger.warn("Invalid index. Wrong list selected?");
+			_logger.warn("Invalid index. Wrong list selected?"); //$NON-NLS-1$
 		}
 	}
 
+	private String getTaskName() {
+		JTextField txtTaskName = new JTextField();
+		JOptionPane.showMessageDialog(null, txtTaskName, Messages.getString("TXT_ENTER_TASK_NAME"), JOptionPane.INFORMATION_MESSAGE); //$NON-NLS-1$
+		return txtTaskName.getText();
+	}
+	
 	public JList getPluginList()
 	{
 		return _pluginList;
@@ -113,7 +133,7 @@ class TaskEditionManager
 				_taskListModel.add(index + 1, task);
 				_taskList.setSelectedIndex(index + 1);
 			} else {
-				_logger.warn("Nothing or wrong index selected");
+				_logger.warn("Nothing or wrong index selected"); //$NON-NLS-1$
 			}
 		}
 	}
@@ -131,7 +151,7 @@ class TaskEditionManager
 				_taskListModel.add(index - 1, task);
 				_taskList.setSelectedIndex(index - 1);
 			} else {
-				_logger.warn("Nothing or wrong index selected");
+				_logger.warn("Nothing or wrong index selected"); //$NON-NLS-1$
 			}
 		}
 	}
@@ -146,9 +166,9 @@ class TaskEditionManager
 		int index = _taskList.getSelectedIndex();
 		if (index >= 0) {
 			Task task = (Task) _taskListModel.remove(index);
-			System.out.println("plugin = " + task);
+			System.out.println("plugin = " + task); //$NON-NLS-1$
 		} else {
-			_logger.warn("Invalid index. Wrong list selected?");
+			_logger.warn("Invalid index. Wrong list selected?"); //$NON-NLS-1$
 		}
 	}
 
@@ -156,14 +176,11 @@ class TaskEditionManager
 	{
 		if (_pluginPopup == null) {
 			_pluginPopup = new JPopupMenu();
-			JMenuItem itmInfo = new JMenuItem("Info");
+			JMenuItem itmInfo = new JMenuItem(Messages.getString("MNU_INFO")); //$NON-NLS-1$
 			itmInfo.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e)
 				{
-					// TODO:
-					String info = _pluginListModel.get(_selectedItem)
-							.toString();
-					JOptionPane.showMessageDialog(null, info);
+					showPluginInfo();
 				}
 			});
 			_pluginPopup.add(itmInfo);
@@ -175,7 +192,7 @@ class TaskEditionManager
 	{
 		if (_taskPopup == null) {
 			_taskPopup = new JPopupMenu();
-			JMenuItem itmSettings = new JMenuItem("Settings");
+			JMenuItem itmSettings = new JMenuItem(Messages.getString("MNU_SETTINGS")); //$NON-NLS-1$
 			itmSettings.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e)
 				{
@@ -185,32 +202,86 @@ class TaskEditionManager
 					ISettingComponent settingComponent = plugin
 							.getSettingComponent();
 					int result = JOptionPane.showConfirmDialog(null,
-							settingComponent.getComponent(), "Plugin settings",
+							settingComponent.getComponent(), Messages.getString("TIT_PLUGIN_SETTINGS"), //$NON-NLS-1$
 							JOptionPane.OK_CANCEL_OPTION);
 					if (result == JOptionPane.OK_OPTION) {
 						ISettings settings = settingComponent.getSettings();
-						_logger.info("settings: " + settings);
+						_logger.info("settings: " + settings); //$NON-NLS-1$
 						currentTask.setSettings(settings);
 					}
 				}
 			});
-			JMenuItem itmResult = new JMenuItem("Result");
+			JMenuItem itmResult = new JMenuItem(Messages.getString("MNU_RESULT")); //$NON-NLS-1$
 			itmResult.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e)
 				{
 					// TODO:
 					Task currentTask = (Task) _taskListModel.get(_selectedItem);
 					IPlugin plugin = currentTask.getPlugin();
-					IResultComponent resultComponent = plugin.getResultComponent();
-					JOptionPane.showMessageDialog(null,
-							resultComponent.getComponent(currentTask.getResult()), "Plugin result",
-							JOptionPane.INFORMATION_MESSAGE);
+					IResultComponent resultComponent = plugin
+							.getResultComponent();
+					JOptionPane.showMessageDialog(null, resultComponent
+							.getComponent(currentTask.getResult()),
+							Messages.getString("TIT_PLUGIN_RESULT"), JOptionPane.INFORMATION_MESSAGE); //$NON-NLS-1$
 				}
 			});
 			_taskPopup.add(itmSettings);
 			_taskPopup.add(itmResult);
 		}
 		return _taskPopup;
+	}
+
+	private void showErrorMessage(String msg)
+	{
+		JOptionPane.showMessageDialog(null, msg, Messages.getString("TIT_ERROR"), //$NON-NLS-1$
+				JOptionPane.ERROR_MESSAGE);
+	}
+
+	private void showPluginInfo()
+	{
+		LocalPlugin plugin = (LocalPlugin) _pluginListModel.get(_selectedItem);
+		String info;
+		_logger.debug("plugin: " + plugin); //$NON-NLS-1$
+		try {
+			info = plugin.getPlugin().getDescription().getInfo();
+			JOptionPane.showMessageDialog(null, info);
+		} catch (Exception e) {
+			_logger.fatal("", e);			 //$NON-NLS-1$
+			showErrorMessage(Messages.getString("ERR_CANNOT_LOAD_PLUGIN")); //$NON-NLS-1$
+		}		
+	}
+
+	/** Class helps managing plugin loading */
+	private class LocalPlugin
+	{
+		private IPlugin _plugin = null;
+
+		private File _pluginFile = null;
+
+		public LocalPlugin(File pluginFile)
+		{
+			_pluginFile = pluginFile;
+		}
+
+		/**
+		 * Method returns plugin. If it is not loaded, tries load it using
+		 * PluginLoader
+		 * 
+		 * @throws Exception
+		 */
+		public IPlugin getPlugin() throws Exception
+		{
+			if (_plugin == null) {
+				_logger.debug("trying to load plugin"); //$NON-NLS-1$
+				_plugin = PluginLoader.loadPlugin(_pluginFile);
+			}
+			return _plugin;
+		}
+		
+		public String toString()
+		{			
+			return _pluginFile.toString();
+		}
 	}
 
 	private class PopupListener extends MouseAdapter
