913a1688d2413132eac3da4c8637101e6536df5d
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-23 14:07:54 +0000

    Name field added.

--- src/salomon/core/task/Task.java
+++ src/salomon/core/task/Task.java
@@ -14,12 +14,22 @@ import salomon.plugin.ISettings;
  */
 public class Task
 {
+	private String _name;
+
 	private IPlugin _plugin;
+
+	private IResult _result;
 
 	private ISettings _settings;
 
-	private IResult _result;
-	
+	/**
+	 * @return Returns the name.
+	 */
+	public String getName()
+	{
+		return _name;
+	}
+
 	/**
 	 * @return Returns the _plugin.
 	 */
@@ -27,34 +37,7 @@ public class Task
 	{
 		return _plugin;
 	}
-	/**
-	 * @param _plugin The _plugin to set.
-	 */
-	public void setPlugin(IPlugin plugin)
-	{
-		_plugin = plugin;
-	}
-	/**
-	 * @return Returns the _settings.
-	 */
-	public ISettings getSettings()
-	{
-		return _settings;
-	}
-	/**
-	 * @param _settings The _settings to set.
-	 */
-	public void setSettings(ISettings settings)
-	{
-		_settings = settings;
-	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	public String toString()
-	{		
-		return super.toString() + "[" + _plugin + "]";
-	}
+
 	/**
 	 * @return Returns the _result.
 	 */
@@ -62,11 +45,58 @@ public class Task
 	{
 		return _result;
 	}
+
 	/**
-	 * @param _result The _result to set.
+	 * @return Returns the _settings.
+	 */
+	public ISettings getSettings()
+	{
+		return _settings;
+	}
+
+	/**
+	 * @param name
+	 *            The name to set.
+	 */
+	public void setName(String name)
+	{
+		this._name = name;
+	}
+
+	/**
+	 * @param _plugin
+	 *            The _plugin to set.
+	 */
+	public void setPlugin(IPlugin plugin)
+	{
+		_plugin = plugin;
+	}
+
+	/**
+	 * @param _result
+	 *            The _result to set.
 	 */
 	public void setResult(IResult result)
 	{
 		_result = result;
+	}
+
+	/**
+	 * @param _settings
+	 *            The _settings to set.
+	 */
+	public void setSettings(ISettings settings)
+	{
+		_settings = settings;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString()
+	{
+		return _name + " [" + _plugin + "]";
 	}
 } // end Task
