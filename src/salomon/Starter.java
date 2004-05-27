d748a12d8d4acc2f399bb946c4fa6b63fcad9716
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-27 19:25:23 +0000

    Cosmetics.

--- src/salomon/Starter.java
+++ src/salomon/Starter.java
@@ -1,20 +1,13 @@
-/** Java class "Starter.java" generated from Poseidon for UML.
- *  Poseidon for UML is developed by <A HREF="http://www.gentleware.com">Gentleware</A>.
- *  Generated with <A HREF="http://jakarta.apache.org/velocity/">velocity</A> template engine.
- */
 
 package salomon;
 
 import java.sql.SQLException;
-
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
-
 import salomon.controller.IController;
 import salomon.controller.LocalController;
 import salomon.core.ManagerEngine;
 import salomon.core.data.DBManager;
-import salomon.core.plugin.PluginLoader;
 
 /**
  *  
@@ -52,22 +45,10 @@ public final class Starter
 	{
 		initManagers();
 		_manager.start(_managerEngine);
-		//testLoadPlugin();
 	}
 
 	public static void main(String[] args)
 	{
 		(new Starter()).start();
 	}
-	// OK
-	private void testLoadPlugin()
-	{
-		try {
-			Object plugin = PluginLoader
-					.loadPlugin("plugins/AveragePrice.jar");
-			_logger.debug(plugin);
-		} catch (Exception e) {
-			_logger.fatal("", e);
-		}
-	}
-} // end Starter
+} 
