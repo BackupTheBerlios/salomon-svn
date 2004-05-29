515e98cea6893c768a15a442896f492f84db0b53
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-29 19:41:10 +0000

    Some tests added.

--- src/salomon/Starter.java
+++ src/salomon/Starter.java
@@ -4,21 +4,24 @@ package salomon;
 import java.sql.SQLException;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import pl.edu.agh.icsr.salomon.plugin.averageprice.APSettings;
 import salomon.controller.IController;
 import salomon.controller.LocalController;
 import salomon.core.ManagerEngine;
 import salomon.core.data.DBManager;
+import salomon.core.data.common.DBColumnName;
+import salomon.core.data.common.DBCondition;
+import salomon.core.data.common.DBTableName;
+import salomon.core.data.common.DBValue;
 
 /**
  *  
  */
 public final class Starter
 {
-	private IController _manager;
+	private IController _contoroller;
 
 	private ManagerEngine _managerEngine;
-
-	private DBManager _dbConnector;
 
 	private static Logger _logger = Logger.getLogger(Starter.class);
 
@@ -30,25 +33,127 @@ public final class Starter
 
 	private void initManagers()
 	{
-		_manager = new LocalController();
+		_contoroller = new LocalController();
 		_managerEngine = new ManagerEngine();
-		try {
-			_dbConnector = DBManager.getInstance();
-		} catch (SQLException e) {
-			_logger.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			_logger.fatal("", e);
-		}
 	}
 
 	public void start()
 	{
-		initManagers();
-		_manager.start(_managerEngine);
+		//		initManagers();
+		//		_contoroller.start(_managerEngine);
+		//		try {
+		//			new ProjectManager().loadProject(1);
+		//		} catch (Exception e) {
+		//			_logger.fatal("", e);
+		//		}
+		//testSettings();
+		//testInsert();
+		//testAutoInsert();
+		//testDelete();
+		testUpdate();
+		
 	}
 
 	public static void main(String[] args)
 	{
 		(new Starter()).start();
 	}
-} 
+
+	private void testSettings()
+	{
+		APSettings settings = new APSettings();
+		_logger.debug("test setting");
+		settings.setNick("nico");
+		_logger.debug(settings.getSettings());
+		_logger.debug(settings.toString());
+		_logger.debug("test parsing");
+		settings = new APSettings();
+		settings.parseSettings(";;;");
+		_logger.debug(settings.getSettings());
+		_logger.debug(settings.toString());
+	}
+
+	private void testInsert()
+	{
+		DBTableName tableName = new DBTableName("tasks");
+		DBValue[] values = {				
+				new DBValue(new DBColumnName(tableName, "project_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "plugin_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "name"), "test name",
+						DBValue.TEXT),
+				new DBValue(new DBColumnName(tableName, "info"), "test info",
+						DBValue.TEXT),
+				new DBValue(new DBColumnName(tableName, "plugin_settings"),
+						"test settings", DBValue.TEXT),
+				new DBValue(new DBColumnName(tableName, "plugin_result"),
+						"test result", DBValue.TEXT)};
+		try {
+			DBManager.getInstance().insert(values);
+		} catch (SQLException e) {
+			_logger.fatal("", e);
+		} catch (ClassNotFoundException e) {
+			_logger.fatal("", e);
+		}
+	}
+	private void testAutoInsert()
+	{
+		DBTableName tableName = new DBTableName("tasks");
+		DBValue[] values = {				
+				new DBValue(new DBColumnName(tableName, "project_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "plugin_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "name"), "test name auto inc",
+						DBValue.TEXT),
+				};		 
+		try {
+			DBManager.getInstance().insert(values, "task_id");
+		} catch (SQLException e) {
+			_logger.fatal("", e);
+		} catch (ClassNotFoundException e) {
+			_logger.fatal("", e);
+		}
+	}
+	private void testDelete()
+	{
+		DBTableName tableName = new DBTableName("tasks");
+		DBCondition[] conditions = {
+				new DBCondition(new DBColumnName(tableName, "task_id"),
+						DBCondition.REL_M, new Integer(1), DBCondition.NUMBERIC),
+				new DBCondition(new DBColumnName(tableName, "plugin_id"),
+						DBCondition.REL_NEQ, new Integer(2), DBCondition.NUMBERIC)};
+		try {
+			DBManager.getInstance().delete(conditions);			
+		} catch (SQLException e) {			
+			_logger.fatal("",e);
+		} catch (ClassNotFoundException e) {			
+			_logger.fatal("",e);
+		}
+	}
+	private void testUpdate()
+	{
+		DBTableName tableName = new DBTableName("tasks");
+		DBValue[] values = {				
+				new DBValue(new DBColumnName(tableName, "project_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "plugin_id"),
+						new Integer(2), DBValue.NUMBERIC),
+				new DBValue(new DBColumnName(tableName, "name"), "test name",
+						DBValue.TEXT)};
+		DBCondition[] conditions = {
+				new DBCondition(new DBColumnName(tableName, "task_id"),
+						DBCondition.REL_M, new Integer(1), DBCondition.NUMBERIC),
+				new DBCondition(new DBColumnName(tableName, "plugin_id"),
+						DBCondition.REL_NEQ, new Integer(2), DBCondition.NUMBERIC)};
+		try {
+			DBManager.getInstance().update(values, conditions);
+			DBManager.getInstance().update(values, null);
+		} catch (SQLException e) {
+			_logger.fatal("", e);
+		} catch (ClassNotFoundException e) {
+			_logger.fatal("", e);
+		}
+	}
+}
