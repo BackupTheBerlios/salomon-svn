75e68d0448c8db230bb8b904280accf676c25225
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-29 19:50:36 +0000

    First implementation.

--- src/salomon/core/Project.java
+++ src/salomon/core/Project.java
@@ -10,10 +10,88 @@ package salomon.core;
  */
 public class Project
 {
-	///////////////////////////////////////
-	// associations
 	/**
-	 *  
+	 * @return Returns the projectID.
 	 */
-	public ProjectManager knowledgeSystemManager;
-} // end KnowledgeSystem
+	public int getProjectID()
+	{
+		return _projectID;
+	}
+
+	/**
+	 * @param projectId
+	 *            The projectID to set.
+	 */
+	public void setProjectID(int projectId)
+	{
+		_projectID = projectId;
+	}
+
+	private int _projectID = 0;
+
+	private String _name = null;
+
+	private String _info = null;
+
+	private ManagerEngine _managerEngine = null;
+
+	public Project(int projectID)
+	{
+		_projectID = projectID;
+	}
+
+	public Project()
+	{
+	}
+
+	/**
+	 * @return Returns the managerEngine.
+	 */
+	public ManagerEngine getManagerEngine()
+	{
+		return _managerEngine;
+	}
+
+	/**
+	 * @param managerEngine
+	 *            The managerEngine to set.
+	 */
+	public void setManagerEngine(ManagerEngine managerEngine)
+	{
+		_managerEngine = managerEngine;
+	}
+
+	/**
+	 * @return Returns the info.
+	 */
+	public String getInfo()
+	{
+		return _info;
+	}
+
+	/**
+	 * @param info
+	 *            The info to set.
+	 */
+	public void setInfo(String info)
+	{
+		_info = info;
+	}
+
+	/**
+	 * @return Returns the name.
+	 */
+	public String getName()
+	{
+		return _name;
+	}
+
+	/**
+	 * @param name
+	 *            The name to set.
+	 */
+	public void setName(String name)
+	{
+		_name = name;
+	}
+}
