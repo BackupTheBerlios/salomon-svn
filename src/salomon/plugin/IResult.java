6394e9db8caa12d7688690f416cad20815f4eee1
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-31 16:49:16 +0000

    New method added.

--- src/salomon/plugin/IResult.java
+++ src/salomon/plugin/IResult.java
@@ -2,13 +2,30 @@
  * Created on 2004-05-21
  *
  */
+
 package salomon.plugin;
 
 /**
- * @author nico
- * Represents result of plugin execution.
+ * @author nico Represents result of plugin execution.
  */
 public interface IResult
 {
+	/**
+	 * Method parses result from their string representation.
+	 * 
+	 * @param result
+	 */
+	void parseResult(String result);
 
+	/**
+	 * 
+	 * @return String representation of result
+	 */
+	String resultToString();
+
+	/**
+	 * 
+	 * @return true if plugin successfully finished, false otherwise
+	 */
+	boolean isSuccessful();
 }
