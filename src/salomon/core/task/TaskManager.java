7b56783a6e4fb3011e941332d7db0125d25300f2
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-31 16:51:39 +0000

    Reimplementation of run() - task's state support added.

--- src/salomon/core/task/TaskManager.java
+++ src/salomon/core/task/TaskManager.java
@@ -5,12 +5,16 @@
 
 package salomon.core.task;
 
+import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.log4j.Logger;
+import salomon.core.ProjectManager;
+import salomon.core.data.DBManager;
 import salomon.core.data.DataEngine;
 import salomon.core.data.Environment;
+import salomon.plugin.IPlugin;
 import salomon.plugin.IResult;
 import salomon.plugin.ISettings;
 
@@ -19,15 +23,17 @@ import salomon.plugin.ISettings;
  */
 public final class TaskManager
 {
-	private LinkedList _tasks;
-
-	private TaskEngine _taskEngine;
+	private static Logger _logger = Logger.getLogger(TaskManager.class);
 
 	private DataEngine _dataEngine;
 
 	private Environment _environment;
 
-	private static Logger _logger = Logger.getLogger(TaskManager.class);
+	private TaskEngine _taskEngine;
+
+	private LinkedList _tasks;
+
+	private ProjectManager _projectManger;
 
 	public TaskManager()
 	{
@@ -40,15 +46,9 @@ public final class TaskManager
 		_environment.put("CURRENT_DATA_SET", "all_data");
 	}
 
-	public void start()
+	public void addAllTasks(List tasks)
 	{
-		//_taskEngine.start();
-		new TaskEngine().start();
-	}
-
-	public void clearTaskList()
-	{
-		_tasks.clear();
+		_tasks.addAll(tasks);
 	}
 
 	public void addTask(Task task)
@@ -59,15 +59,9 @@ public final class TaskManager
 		//		_tasks.notifyAll();
 	} // end addTask
 
-	public void addAllTasks(List tasks)
+	public void clearTaskList()
 	{
-		_tasks.addAll(tasks);
-	}
-
-	public Task[] getTasks()
-	{
-		Task[] tasks = new Task[_tasks.size()];
-		return (Task[]) _tasks.toArray(tasks);
+		_tasks.clear();
 	}
 
 	public Task getCurrentTask()
@@ -77,8 +71,84 @@ public final class TaskManager
 		//if (_tasks.)
 	} // end getCurrentTask
 
+	public List getTasks()
+	{		
+		return  _tasks;
+	}
+
+	public void start()
+	{
+		//_taskEngine.start();
+		new TaskEngine().start();
+	}
+
 	private final class TaskEngine extends Thread
 	{
+		public void run()
+		{
+			/*
+			 * while (true) { Task task = getTask(); IDataPlugin plugin =
+			 * task.getPlugin(); Settings settings = task.getSettings();
+			 * plugin.doJob(_dataEngine, _environment, settings);
+			 */
+			int projectId = _projectManger.getCurrentProject().getProjectID();
+			for (Iterator iter = _tasks.iterator(); iter.hasNext();) {
+				Task task = (Task) iter.next();
+				ISettings settings = task.getSettings();
+				task.setStatus(Task.REALIZATION);
+				try {
+					// changing status
+					_projectManger.updateTasks(new Task[]{task}, projectId);
+					try {
+						DBManager.getInstance().commit();
+					} catch (ClassNotFoundException e1) {
+						_logger.fatal("", e1);
+					} catch (SQLException e1) {
+						_logger.fatal("", e1);
+					}
+					//
+					// processing task
+					//
+					IPlugin plugin = task.getPlugin();
+					_logger.debug("plugin: " + plugin + " id: " + plugin.getDescription().getPluginID());
+					IResult result = plugin.doJob(_dataEngine,
+							_environment, settings);
+					//
+					// saving result of its execution
+					//
+					task.setResult(result);
+					_projectManger.updateTasks(new Task[]{task}, projectId);
+					try {
+						DBManager.getInstance().commit();
+					} catch (ClassNotFoundException e1) {
+						_logger.fatal("", e1);
+					} catch (SQLException e1) {
+						_logger.fatal("", e1);
+					}
+					//
+					// if task was not processed correctly
+					// exception is caught and shoul be handled here
+					//
+				} catch (Exception e) {
+					_logger.fatal("TASK PROCESSING ERROR", e);
+					task.setStatus(Task.EXCEPTION);
+					try {
+						_projectManger.updateTasks(new Task[]{task}, projectId);
+					} catch (SQLException e1) {
+						_logger.fatal("", e1);
+					}
+					try {
+						DBManager.getInstance().commit();
+					} catch (ClassNotFoundException e1) {
+						_logger.fatal("", e1);
+					} catch (SQLException e1) {
+						_logger.fatal("", e1);
+					}
+				}
+			}
+			//_tasks.clear();
+		}
+
 		private Task getTask()
 		{
 			Task currentTask = null;
@@ -101,28 +171,14 @@ public final class TaskManager
 			} //while (currentTask != null);
 			return currentTask;
 		}
+	}
 
-		public void run()
-		{
-			/*
-			 * while (true) { Task task = getTask(); IDataPlugin plugin =
-			 * task.getPlugin(); Settings settings = task.getSettings();
-			 * plugin.doJob(_dataEngine, _environment, settings);
-			 */
-			for (Iterator iter = _tasks.iterator(); iter.hasNext();) {
-				Task task = (Task) iter.next();
-				ISettings settings = task.getSettings();
-				//TODO:
-				if (settings == null) {
-					_logger.error("settings for task: " + task + " are NULL ");
-					_logger.error("creating empty settings");
-				}
-				IResult result = task.getPlugin().doJob(_dataEngine,
-						_environment, settings);
-				task.setResult(result);
-				// removing task after execution?
-			}
-			//_tasks.clear();
-		}
+	/**
+	 * @param projectManger
+	 *            The projectManger to set.
+	 */
+	public void setProjectManger(ProjectManager projectManger)
+	{
+		_projectManger = projectManger;
 	}
 } // end TaskManager
