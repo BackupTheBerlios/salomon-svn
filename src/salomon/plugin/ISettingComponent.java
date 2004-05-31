bf691edb13684891a5a63c8cdc50c4316fe53ac4
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-05-31 16:49:45 +0000

    getDefaultSettings() added.

--- src/salomon/plugin/ISettingComponent.java
+++ src/salomon/plugin/ISettingComponent.java
@@ -2,24 +2,33 @@
  * Created on 2004-05-22
  *
  */
+
 package salomon.plugin;
 
 import java.awt.Component;
 
 /**
  * @author nico
- *
+ *  
  */
 public interface ISettingComponent
 {
-	/** Returns settings object. 
-	  * It is set basing user selections in setting panel*/
+	/**
+	 * Returns settings object. It is set basing on user selections in setting
+	 * panel
+	 */
 	ISettings getSettings();
+
+	/**
+	 * Method create default settings for plugin.
+	 * 
+	 * @return default settings for plugin
+	 */
+	ISettings getDefaultSettings();
 
 	/** Returns component, which is used by user to set plugin settings */
 	Component getComponent();
 
 	/** Fills settings component basing on settings */
 	Component getComponent(ISettings settings);
-	
 }
