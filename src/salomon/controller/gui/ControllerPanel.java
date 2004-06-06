fe852e113439c77e7ecf589391c440fb511cd2bb
iju <iju@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-06-06 13:16:57 +0000

    Original source file.

---
+++ src/salomon/controller/gui/ControllerPanel.java
@@ -0,0 +1,292 @@
+
+package salomon.controller.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JToolBar;
+import javax.swing.border.TitledBorder;
+
+import org.apache.log4j.Logger;
+
+import salomon.controller.gui.action.ActionManager;
+import salomon.core.Config;
+import salomon.core.Messages;
+import salomon.core.project.Project;
+
+/**
+ * 
+ * TODO: add comment.
+ * 
+ * @author krzychu
+ *  
+ */
+public final class ControllerPanel extends JPanel
+{
+
+	private ActionManager _actionManager;
+
+	private JButton _btnApply;
+
+	private JButton _btnRun;
+
+	private JPanel _contentPane;
+
+	private GUIButtons _guiButtons;
+
+	private JList _lstPlugins;
+
+	private JList _lstTasks;
+
+	private JPanel _pnlInit;
+
+	private JPanel _pnlManagerButtons;
+
+	private JPanel _pnlPluginButtons;
+
+	private JPanel _pnlPlugins;
+
+	private JPanel _pnlTaskButtons;
+
+	private JPanel _pnlTasks;
+
+	private String _resourcesDir;
+
+	private int _strutHeight = 10;
+
+	private int _strutWidth = 10;
+
+	private TaskEditionManager _taskEditionManager;
+
+	private JToolBar _toolBar;
+
+	public ControllerPanel(TaskEditionManager taskEditionManager,
+			ActionManager actionManager)
+	{
+		super();
+		_actionManager = actionManager;
+		_taskEditionManager = taskEditionManager;
+		_resourcesDir = Config.getString("RESOURCES_DIR");
+		_guiButtons = new GUIButtons(_taskEditionManager);
+		this.setLayout(new BorderLayout());
+		this.add(getPnlManagerButtons(), BorderLayout.SOUTH);
+		this.add(getPnlInit(), BorderLayout.CENTER);
+
+	}
+
+	/**
+	 * @return Returns the taskEditionManager.
+	 */
+	public TaskEditionManager getTaskEditionManager()
+	{
+		return _taskEditionManager;
+	}
+
+	/**
+	 * This method initializes _menuBar
+	 * 
+	 * @return
+	 */
+
+	/**
+	 * Called after project save/load/creation
+	 * 
+	 * @param project
+	 */
+	public void refreshGui(Project project)
+	{
+		_taskEditionManager.reloadTasks(project.getManagerEngine().getTasksManager().getTasks());
+	}
+
+	/**
+	 * @param taskEditionManager
+	 *            The taskEditionManager to set.
+	 */
+	public void setTaskEditionManager(TaskEditionManager taskEditionManager)
+	{
+		_taskEditionManager = taskEditionManager;
+	}
+
+	/**
+	 * This method initializes _btnRun
+	 * 
+	 * @return JButton
+	 */
+	JButton getBtnRun()
+	{
+		if (_btnRun == null) {
+			_btnRun = new JButton(_actionManager.getRunTaskAction());
+			_btnRun.setText(Messages.getString("BTN_RUN")); //$NON-NLS-1$
+		}
+		return _btnRun;
+	}
+
+	/**
+	 * This method initializes _btnApply
+	 * 
+	 * @return JButton
+	 */
+	private JButton getBtnApply()
+	{
+		if (_btnApply == null) {
+			_btnApply = new JButton(_actionManager.getSaveProjectAction());
+			_btnApply.setText(Messages.getString("BTN_APPLY")); //$NON-NLS-1$
+		}
+		return _btnApply;
+	}
+
+	/**
+	 * This method initializes _lstPlugins
+	 * 
+	 * @return JList
+	 */
+	private JList getLstPlugins()
+	{
+		if (_lstPlugins == null) {
+			_lstPlugins = _taskEditionManager.getPluginList();
+			_lstPlugins.setBorder(BorderFactory.createLoweredBevelBorder());
+			_lstPlugins.setPreferredSize(new Dimension(100, 200));
+		}
+		return _lstPlugins;
+	}
+
+	/**
+	 * This method initializes _lstTasks
+	 * 
+	 * @return JList
+	 */
+	private JList getLstTasks()
+	{
+		if (_lstTasks == null) {
+			_lstTasks = _taskEditionManager.getTaskList();
+			_lstTasks.setBorder(BorderFactory.createLoweredBevelBorder());
+			_lstTasks.setPreferredSize(new Dimension(100, 200));
+		}
+		return _lstTasks;
+	}
+
+	private JPanel getPnlInit()
+	{
+		if (_pnlInit == null) {
+			_pnlInit = new JPanel();
+			_pnlInit.setLayout(new GridLayout(1, 2));
+			_pnlInit.add(getPnlPlugins());
+			_pnlInit.add(getPnlTasks());
+		}
+		return _pnlInit;
+	}
+
+	/**
+	 * This method initializes _pnlManagerButtons
+	 * 
+	 * @return JPanel
+	 */
+	private JPanel getPnlManagerButtons()
+	{
+		if (_pnlManagerButtons == null) {
+			_pnlManagerButtons = new JPanel();
+			_pnlManagerButtons.setLayout(new BoxLayout(_pnlManagerButtons,
+					BoxLayout.X_AXIS));
+			_pnlManagerButtons.add(Box.createHorizontalGlue());
+			_pnlManagerButtons.add(getBtnApply());
+			_pnlManagerButtons.add(Box.createHorizontalStrut(_strutWidth));
+			_pnlManagerButtons.add(getBtnRun());
+			_pnlManagerButtons.add(Box.createHorizontalGlue());
+		}
+		return _pnlManagerButtons;
+	}
+
+	/**
+	 * This method initializes _pnlPluginButtons
+	 * 
+	 * @return JPanel
+	 */
+	private JPanel getPnlPluginButtons()
+	{
+		if (_pnlPluginButtons == null) {
+			_pnlPluginButtons = new JPanel();
+			_pnlPluginButtons.setLayout(new BoxLayout(_pnlPluginButtons,
+					BoxLayout.Y_AXIS));
+			_pnlPluginButtons.add(Box.createVerticalGlue());
+			_pnlPluginButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlPluginButtons.add(_guiButtons.getBtnAdd());
+			_pnlPluginButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlPluginButtons.add(_guiButtons.getBtnRemove());
+			_pnlPluginButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlPluginButtons.add(_guiButtons.getBtnRemoveAll());
+			_pnlPluginButtons.add(Box.createVerticalGlue());
+		}
+		return _pnlPluginButtons;
+	}
+
+	/**
+	 * This method initializes _pnlPlugins
+	 * 
+	 * @return JPanel
+	 */
+	private JPanel getPnlPlugins()
+	{
+		if (_pnlPlugins == null) {
+			_pnlPlugins = new JPanel();
+			_pnlPlugins.setLayout(new BorderLayout());
+			_pnlPlugins.add(getPnlPluginButtons(), BorderLayout.EAST);
+			_pnlPlugins.add(getLstPlugins(), BorderLayout.CENTER);
+			_pnlPlugins.setBorder(BorderFactory.createTitledBorder(null,
+					Messages.getString("TIT_PLUGINS"), TitledBorder.LEFT, //$NON-NLS-1$
+					TitledBorder.DEFAULT_POSITION));
+		}
+		return _pnlPlugins;
+	}
+
+	/**
+	 * This method initializes _pnlTaskButtons
+	 * 
+	 * @return JPanel
+	 */
+	private JPanel getPnlTaskButtons()
+	{
+		if (_pnlTaskButtons == null) {
+			_pnlTaskButtons = new JPanel();
+			_pnlTaskButtons.setLayout(new BoxLayout(_pnlTaskButtons,
+					BoxLayout.Y_AXIS));
+			_pnlTaskButtons.add(Box.createVerticalGlue());
+			_pnlTaskButtons.add(_guiButtons.getBtnFirst());
+			_pnlTaskButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlTaskButtons.add(_guiButtons.getBtnUp());
+			_pnlTaskButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlTaskButtons.add(_guiButtons.getBtnDown());
+			_pnlTaskButtons.add(Box.createVerticalStrut(_strutHeight));
+			_pnlTaskButtons.add(_guiButtons.getBtnLast());
+			_pnlTaskButtons.add(Box.createVerticalGlue());
+		}
+		return _pnlTaskButtons;
+	}
+
+	/**
+	 * This method initializes _pnlTasks
+	 * 
+	 * @return JPanel
+	 */
+	private JPanel getPnlTasks()
+	{
+		if (_pnlTasks == null) {
+			_pnlTasks = new JPanel();
+			_pnlTasks.setLayout(new BorderLayout());
+			_pnlTasks.add(getPnlTaskButtons(), BorderLayout.EAST);
+			_pnlTasks.add(getLstTasks(), BorderLayout.CENTER);
+			_pnlTasks.setBorder(BorderFactory.createTitledBorder(null,
+					Messages.getString("TIT_TASKS"), //$NON-NLS-1$
+					TitledBorder.LEFT, TitledBorder.DEFAULT_POSITION));
+		}
+		return _pnlTasks;
+	}
+
+	private static Logger _logger = Logger.getLogger(ControllerFrame.class);
+}
