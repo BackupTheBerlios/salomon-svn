64803e7e82339c09402aa4b58d62fdf3b855ebb2
iju <iju@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-07-03 21:32:18 +0000

    Moved from PluginLoader.

---
+++ src/salomon/core/plugin/PluginClassLoader.java
@@ -0,0 +1,60 @@
+
+package salomon.core.plugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.jar.JarFile;
+
+import org.apache.log4j.Logger;
+
+final class PluginClassLoader extends URLClassLoader
+{
+	PluginClassLoader() throws MalformedURLException
+	{
+		super(new URL[0]);
+	}
+
+	//work around
+	public Class findMainClass(File file) throws ClassNotFoundException,
+			IOException, URISyntaxException
+	{
+		Class foundClass = null;
+		_logger.debug("file: " + file);
+		JarFile jarFile = new JarFile(file);
+		_logger.debug("jarFile: " + jarFile);
+		// reading manifest to find main class
+		String className = jarFile.getManifest().getMainAttributes().getValue(
+				"Main-Class");
+		_logger.info("Looking for class: " + className);
+		foundClass = super.loadClass(className);
+		_logger.info("Found class: " + foundClass);
+
+		return foundClass;
+	}
+
+	void addUrl(URL url)
+	{
+		super.addURL(url);
+	}
+
+	static PluginClassLoader getInstance()
+	{
+		if (_instance == null) {
+			try {
+				_instance = new PluginClassLoader();
+			} catch (MalformedURLException e) {
+				_logger.fatal("", e);
+			}
+		}
+
+		return _instance;
+	}
+
+	private static PluginClassLoader _instance;
+
+	private static Logger _logger = Logger.getLogger(PluginClassLoader.class);
+}
