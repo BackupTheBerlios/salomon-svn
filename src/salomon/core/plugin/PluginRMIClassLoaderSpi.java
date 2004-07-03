7f733d2163a07ec0962054ddbe54d61614e432e6
iju <iju@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-07-03 21:33:37 +0000

    Original source code.

---
+++ src/salomon/core/plugin/PluginRMIClassLoaderSpi.java
@@ -0,0 +1,79 @@
+
+package salomon.core.plugin;
+
+import java.lang.reflect.Proxy;
+import java.net.MalformedURLException;
+import java.rmi.server.RMIClassLoaderSpi;
+
+import org.apache.log4j.Logger;
+
+public final class PluginRMIClassLoaderSpi extends RMIClassLoaderSpi
+{
+	/**
+	 *  
+	 */
+	public PluginRMIClassLoaderSpi()
+	{
+		_logger.debug("Creating PluginRMIClassLoaderSpi");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.rmi.server.RMIClassLoaderSpi#getClassAnnotation(java.lang.Class)
+	 */
+	public String getClassAnnotation(Class cl)
+	{
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.rmi.server.RMIClassLoaderSpi#getClassLoader(java.lang.String)
+	 */
+	public ClassLoader getClassLoader(String codebase)
+			throws MalformedURLException
+	{
+		_logger.error("Not implemented!!!");
+
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.rmi.server.RMIClassLoaderSpi#loadClass(java.lang.String,
+	 *      java.lang.String, java.lang.ClassLoader)
+	 */
+	public Class loadClass(String codebase, String name,
+			ClassLoader defaultLoader) throws MalformedURLException,
+			ClassNotFoundException
+	{
+		return PluginClassLoader.getInstance().loadClass(name);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.rmi.server.RMIClassLoaderSpi#loadProxyClass(java.lang.String,
+	 *      java.lang.String[], java.lang.ClassLoader)
+	 */
+	public Class loadProxyClass(String codebase, String[] interfaces,
+			ClassLoader defaultLoader) throws MalformedURLException,
+			ClassNotFoundException
+	{
+		PluginClassLoader classLoader = PluginClassLoader.getInstance();
+		Class[] interfaceClasses = new Class[interfaces.length];
+		for (int i = 0; i < interfaces.length; ++i) {
+			interfaceClasses[i] = classLoader.loadClass(interfaces[i]);
+		}
+
+		Class proxyClass = Proxy.getProxyClass(PluginClassLoader.getInstance(),
+				interfaceClasses);
+
+		return proxyClass;
+	}
+    
+	private static Logger _logger = Logger.getLogger(PluginRMIClassLoaderSpi.class);
+}
