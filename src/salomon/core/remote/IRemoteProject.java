b64b37f9bd0db76a5bed62b11e4cc4ec1818aae5
iju <iju@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-09-06 21:05:02 +0000

    Contracts added.

--- src/salomon/core/remote/IRemoteProject.java
+++ src/salomon/core/remote/IRemoteProject.java
@@ -14,35 +14,44 @@ import java.rmi.RemoteException;
 public interface IRemoteProject extends Remote
 {
 	/**
-	 * @return Returns the info.
-	 */
-	public String getInfo() throws RemoteException;
+     * @return Returns the info.
+     * @pre $none
+     * @post $result != null
+     */
+	String getInfo() throws RemoteException;
 
 	/**
-	 * @return Returns the name.
-	 */
-	public String getName() throws RemoteException;
+     * @return Returns the name.
+     * @pre $none
+     * @post $result != null
+     */
+	String getName() throws RemoteException;
 
 	/**
-	 * @return Returns the projectID.
-	 */
-	public int getProjectID() throws RemoteException;
+     * @return Returns the projectID.
+     * @pre $none
+     * @post $none
+     */
+	int getProjectID() throws RemoteException;
 
 	/**
-	 * @param info
-	 *            The info to set.
-	 */
-	public void setInfo(String info) throws RemoteException;
+     * @param info The info to set.
+     * @pre info != null
+     * @post $none
+     */
+	void setInfo(String info) throws RemoteException;
 
 	/**
-	 * @param name
-	 *            The name to set.
-	 */
-	public void setName(String name) throws RemoteException;
+     * @param name The name to set.
+     * @pre name != null
+     * @post $none
+     */
+	void setName(String name) throws RemoteException;
 
 	/**
-	 * @param projectId
-	 *            The projectID to set.
-	 */
-	public void setProjectID(int projectId) throws RemoteException;
+     * @param projectId The projectID to set.
+     * @pre $none
+     * @post $none
+     */
+	void setProjectID(int projectId) throws RemoteException;
 }
