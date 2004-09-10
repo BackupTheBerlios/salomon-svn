ce702e1013218566690028381e03f8eea747905a
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-09-10 18:52:27 +0000

    Some methods to visualize query result moves from SQLConsole.

--- src/salomon/controller/gui/Utils.java
+++ src/salomon/controller/gui/Utils.java
@@ -1,8 +1,19 @@
 
 package salomon.controller.gui;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+
+import org.apache.log4j.Logger;
 
 import salomon.core.Messages;
 
@@ -15,6 +26,7 @@ import salomon.core.Messages;
  */
 public class Utils
 {
+
 	private JComponent _parent;
 
 	private void setParentImpl(JComponent parent)
@@ -47,6 +59,96 @@ public class Utils
 				Messages.getString("TIT_WARN"), JOptionPane.YES_NO_OPTION,
 				JOptionPane.WARNING_MESSAGE);
 		return (retVal == JOptionPane.YES_OPTION);
+	}
+
+	public static JTable createResultTable(Collection allData)
+	{
+		String[] columnNames = null;
+		Object[][] data = null;
+
+		int rowCount = 0;
+		for (Iterator iter = allData.iterator(); iter.hasNext();) {
+			Object[] row = (Object[]) iter.next();
+			if (rowCount == 0) {
+				columnNames = (String[]) Arrays.asList(row).toArray(
+						new String[row.length]);
+				// creating matrix for data
+				data = new Object[allData.size() - 1][columnNames.length];
+			} else {
+				data[rowCount - 1] = row;
+			}
+			rowCount++;
+		}
+
+		JTable table = new JTable(data, columnNames);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+		return table;
+	}
+
+	/**
+	 * Method creates JTable representing given result set.
+	 * 
+	 * @param resultSet result of SQL query
+	 * @return table representing given result set.
+	 * @throws SQLException
+	 */
+	public static JTable createResultTable(ResultSet resultSet)
+			throws SQLException
+	{
+		JTable table = createResultTable(getDataFromResultSet(resultSet));
+		return table;
+	}
+
+	/**
+	 * Processes given result set. Returns Collection, which first element is an
+	 * array of column names and a selected rows as next elements
+	 * 
+	 * @param resultSet
+	 * @return @throws SQLException
+	 */
+	public static Collection getDataFromResultSet(ResultSet resultSet)
+			throws SQLException
+	{
+		LinkedList allData = new LinkedList();
+		ResultSetMetaData metaData = resultSet.getMetaData();
+		int columnCount = metaData.getColumnCount();
+		String[] columnNames = new String[columnCount];
+		// getting column names
+		for (int i = 0; i < columnCount; i++) {
+			columnNames[i] = metaData.getColumnLabel(i + 1);
+		}
+		allData.add(columnNames);
+		// getting data
+		int size = 0;
+		while (resultSet.next()) {
+			Object[] row = new Object[columnCount];
+			int i = 0;
+			for (; i < columnCount; i++) {
+				row[i] = resultSet.getObject(i + 1);
+			}
+			allData.add(row);
+			size++;
+		}
+
+		// printing result
+		StringBuffer buffer = new StringBuffer(512);
+		int rowCount = 0;
+		for (Iterator iter = allData.iterator(); iter.hasNext();) {
+			Object[] row = (Object[]) iter.next();
+			for (int i = 0; i < row.length; i++) {
+				buffer.append(row[i] + "|"); //$NON-NLS-1$
+			}
+			if (rowCount == 0) {
+				buffer.append("\n=============================================\n"); //$NON-NLS-1$
+			} else {
+				buffer.append("\n"); //$NON-NLS-1$   
+			}
+			rowCount++;
+			_logger.info(buffer);
+		}
+
+		return allData;
 	}
 
 	public static void setParent(JComponent parent)
@@ -83,4 +185,5 @@ public class Utils
 	}
 
 	private static Utils _instance;
+	private static Logger _logger = Logger.getLogger(Utils.class);
 }
