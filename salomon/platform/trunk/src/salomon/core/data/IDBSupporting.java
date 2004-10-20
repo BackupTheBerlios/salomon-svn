ba740396961c8cc3455ae6f3528739f03d2619cd
niko <niko@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-10-20 18:03:53 +0000

    Interfejs implemented by classes can save/load themselves in/from data base.

---
+++ salomon/platform/trunk/src/salomon/core/data/IDBSupporting.java
@@ -0,0 +1,45 @@
+package salomon.core.data;
+
+import java.net.MalformedURLException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Interface to make database support easier.
+ * Classes which implement it can save/load/delete 
+ * appropriate records from data base. 
+ * 
+ */
+public interface IDBSupporting
+{
+	/**
+	 * Initializes itself basing on given row from resultSet.
+	 * 
+	 * @param resultSet
+	 * @throws SQLException
+	 * @throws MalformedURLException
+	 */
+	public void load(ResultSet resultSet) throws MalformedURLException,
+			SQLException;
+
+	/**
+	 * Saves itself in data base. If already exists in database performs update
+	 * otherwise inserts new record. Returns current id if update was executed
+	 * or new id in case of insert.
+	 * 
+	 * @return unique id
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public int save() throws SQLException, ClassNotFoundException;
+
+	/**
+	 * Removes itself from database.
+	 * After successsful finish object should be destroyed.
+	 * 
+	 * @return
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public boolean delete() throws SQLException, ClassNotFoundException;
+}
