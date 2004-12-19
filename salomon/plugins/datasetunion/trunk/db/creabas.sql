aa4926dcae079c617dca8f0fe1526fb38a3a0e60
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2004-12-19 11:55:08 +0000

    Trigger before insert added to autoincrement person_id.

--- salomon/plugins/datasetunion/trunk/db/creabas.sql
+++ salomon/plugins/datasetunion/trunk/db/creabas.sql
@@ -9,3 +9,20 @@ add constraint PK_PERSONS
 primary key (ID);
 
 CREATE GENERATOR GEN_PERSONS_ID;
+
+-- trigger before insert
+
+SET TERM ^ ;
+
+
+CREATE TRIGGER PERSONS_BI FOR PERSONS
+ACTIVE BEFORE INSERT POSITION 0
+AS
+BEGIN
+  IF (NEW.ID IS NULL) THEN
+    NEW.ID = GEN_ID(gen_persons_id,1);
+END
+^
+
+SET TERM ; ^
+
