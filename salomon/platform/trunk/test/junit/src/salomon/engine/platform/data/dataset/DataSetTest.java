2e9488c71ca52fba7f0a393c4971743aa57b24ef
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2005-05-28 13:46:33 +0000

    Solution test modified - DataSet test added.

--- salomon/platform/trunk/test/junit/src/salomon/engine/platform/data/dataset/DataSetTest.java
+++ salomon/platform/trunk/test/junit/src/salomon/engine/platform/data/dataset/DataSetTest.java
@@ -21,16 +21,12 @@
 
 package salomon.engine.platform.data.dataset;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import salomon.engine.database.DBManager;
-import salomon.engine.database.queries.SQLSelect;
 
 public class DataSetTest extends TestCase
 {
@@ -42,162 +38,164 @@ public class DataSetTest extends TestCase
 	 */
 	DBManager _manager;
 
+	//TODO: commented out until DataSet is implemented
+
 	/*
 	 * Class under test for ResultSet selectData(SQLSelect)
 	 */
-	public void testSelectDataSQLSelect1()
-	{
-		LOGGER.info("DataSetTest.testSelectDataSQLSelect1()");
-		boolean success = false;
-
-		DataSet dataSet = new DataSet("testName");
-		dataSet.addCondition("project", "project_id > 10");
-		dataSet.addCondition("project", "project_name <> 'project_1'");
-		dataSet.addCondition("task", "task_name  'task1'");
-		SQLSelect select = new SQLSelect();
-		select.addTable("projects");
-		select.addTable("tasks");
-		select.addCondition("task_id >", 10);
-		select.addCondition("task_name =", "task2");
-
-		String selectBefore = select.getQuery();
-		LOGGER.debug("select: " + selectBefore);
-		try {
-			ResultSet result = dataSet.selectData(select);
-            result.close();
-            _manager.rollback();
-			ResultSet result2 = dataSet.selectData(select);	
-			result2.close();
-            
-			success = true;
-		} catch (SQLException e) {
-			LOGGER.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			LOGGER.fatal("", e);
-		}
-		_manager.rollback();
-		assertTrue(success);
-		String selectAfter = select.getQuery();
-		LOGGER.debug("select: " + selectAfter);
-		assertEquals(selectBefore, selectAfter);			
-	}
-	
-
-	public void testSelectDataSQLSelect2()
-	{
-		LOGGER.info("DataSetTest.testSelectDataSQLSelect2()");
-		boolean success = false;
-
-		DataSet dataSet = new DataSet("testName");
-		dataSet.addCondition("projects p", "p.project_id > 10");
-		dataSet.addCondition("projects p", "p.project_name <> 'project_1'");
-
-		SQLSelect select = new SQLSelect();
-		select.addTable("projects");
-		select.addTable("tasks");
-		select.addCondition("task_id >", 10);
-		select.addCondition("task_name =", "task2");
-
-		String selectBefore = select.getQuery();
-		LOGGER.debug("select: " + selectBefore);
-		try {
-			ResultSet result = dataSet.selectData(select);
-			result.close();			
-			success = true;			
-		} catch (SQLException e) {
-			LOGGER.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			LOGGER.fatal("", e);
-		}
-		_manager.rollback();
-		assertTrue(success);
-		String selectAfter = select.getQuery();
-		LOGGER.debug("select: " + selectAfter);
-		assertEquals(selectBefore, selectAfter);
-	}
-
-	public void testSelectDataSQLSelect3()
-	{
-		LOGGER.info("DataSetTest.testSelectDataSQLSelect2()");
-		boolean success = false;
-
-		DataSet dataSet = new DataSet("testName");
-
-		SQLSelect select = new SQLSelect();
-		select.addTable("projects");
-		select.addTable("tasks");
-		select.addCondition("task_id >", 10);
-		select.addCondition("task_name =", "task2");
-
-		String selectBefore = select.getQuery();
-		LOGGER.debug("select: " + selectBefore);
-		try {
-			ResultSet result = dataSet.selectData(select);
-			result.close();
-			success = true;
-		} catch (SQLException e) {
-			LOGGER.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			LOGGER.fatal("", e);
-		}
-		_manager.rollback();
-		assertTrue(success);
-		String selectAfter = select.getQuery();
-		LOGGER.debug("select: " + selectAfter);
-		assertEquals(selectBefore, selectAfter);
-	}
-
-	/*
-	 * Class under test for ResultSet selectData(String)
-	 */
-	public void testSave1()
-	{
-		LOGGER.info("DataSetTest.testSave1()");
-		boolean success = false;
-		DataSet dataSet = new DataSet("testName");
-		dataSet.addCondition("projects", "project_id > 10");
-		dataSet.addCondition("projects", "project_name <> 'project_1'");
-		try {
-			dataSet.save();
-			success = true;
-		} catch (SQLException e) {
-			LOGGER.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			LOGGER.fatal("", e);
-		}
-		_manager.rollback();
-		assertTrue(success);
-	}
-	
-	public void testSave2()
-	{
-		LOGGER.info("DataSetTest.testSave2()");
-		boolean success = false;
-		DataSet dataSet = new DataSet("testName2");
-		dataSet.addCondition("projects", "project_id > 10");
-		dataSet.addCondition("projects", "project_name <> 'project_1'");
-		dataSet.addCondition("tasks", "task_id = 2");
-		try {
-			dataSet.save();
-			success = true;
-		} catch (SQLException e) {
-			LOGGER.fatal("", e);
-		} catch (ClassNotFoundException e) {
-			LOGGER.fatal("", e);
-		}
-		_manager.rollback();
-		assertTrue(success);
-	}
-
+	//	public void testSelectDataSQLSelect1()
+	//	{
+	//		LOGGER.info("DataSetTest.testSelectDataSQLSelect1()");
+	//		boolean success = false;
+	//
+	//		DataSet dataSet = 
+	//		dataSet.addCondition("project", "project_id > 10");
+	//		dataSet.addCondition("project", "project_name <> 'project_1'");
+	//		dataSet.addCondition("task", "task_name  'task1'");
+	//		SQLSelect select = new SQLSelect();
+	//		select.addTable("projects");
+	//		select.addTable("tasks");
+	//		select.addCondition("task_id >", 10);
+	//		select.addCondition("task_name =", "task2");
+	//
+	//		String selectBefore = select.getQuery();
+	//		LOGGER.debug("select: " + selectBefore);
+	//		try {
+	//			ResultSet result = dataSet.selectData(select);
+	//            result.close();
+	//            _manager.rollback();
+	//			ResultSet result2 = dataSet.selectData(select);	
+	//			result2.close();
+	//            
+	//			success = true;
+	//		} catch (SQLException e) {
+	//			LOGGER.fatal("", e);
+	//		} catch (ClassNotFoundException e) {
+	//			LOGGER.fatal("", e);
+	//		}
+	//		_manager.rollback();
+	//		assertTrue(success);
+	//		String selectAfter = select.getQuery();
+	//		LOGGER.debug("select: " + selectAfter);
+	//		assertEquals(selectBefore, selectAfter);			
+	//	}
+	//	
+	//
+	//	public void testSelectDataSQLSelect2()
+	//	{
+	//		LOGGER.info("DataSetTest.testSelectDataSQLSelect2()");
+	//		boolean success = false;
+	//
+	//		DataSet dataSet = new DataSet("testName");
+	//		dataSet.addCondition("projects p", "p.project_id > 10");
+	//		dataSet.addCondition("projects p", "p.project_name <> 'project_1'");
+	//
+	//		SQLSelect select = new SQLSelect();
+	//		select.addTable("projects");
+	//		select.addTable("tasks");
+	//		select.addCondition("task_id >", 10);
+	//		select.addCondition("task_name =", "task2");
+	//
+	//		String selectBefore = select.getQuery();
+	//		LOGGER.debug("select: " + selectBefore);
+	//		try {
+	//			ResultSet result = dataSet.selectData(select);
+	//			result.close();			
+	//			success = true;			
+	//		} catch (SQLException e) {
+	//			LOGGER.fatal("", e);
+	//		} catch (ClassNotFoundException e) {
+	//			LOGGER.fatal("", e);
+	//		}
+	//		_manager.rollback();
+	//		assertTrue(success);
+	//		String selectAfter = select.getQuery();
+	//		LOGGER.debug("select: " + selectAfter);
+	//		assertEquals(selectBefore, selectAfter);
+	//	}
+	//
+	//	public void testSelectDataSQLSelect3()
+	//	{
+	//		LOGGER.info("DataSetTest.testSelectDataSQLSelect2()");
+	//		boolean success = false;
+	//
+	//		DataSet dataSet = new DataSet("testName");
+	//
+	//		SQLSelect select = new SQLSelect();
+	//		select.addTable("projects");
+	//		select.addTable("tasks");
+	//		select.addCondition("task_id >", 10);
+	//		select.addCondition("task_name =", "task2");
+	//
+	//		String selectBefore = select.getQuery();
+	//		LOGGER.debug("select: " + selectBefore);
+	//		try {
+	//			ResultSet result = dataSet.selectData(select);
+	//			result.close();
+	//			success = true;
+	//		} catch (SQLException e) {
+	//			LOGGER.fatal("", e);
+	//		} catch (ClassNotFoundException e) {
+	//			LOGGER.fatal("", e);
+	//		}
+	//		_manager.rollback();
+	//		assertTrue(success);
+	//		String selectAfter = select.getQuery();
+	//		LOGGER.debug("select: " + selectAfter);
+	//		assertEquals(selectBefore, selectAfter);
+	//	}
+	//
+	//	/*
+	//	 * Class under test for ResultSet selectData(String)
+	//	 */
+	//	public void testSave1()
+	//	{
+	//		LOGGER.info("DataSetTest.testSave1()");
+	//		boolean success = false;
+	//		DataSet dataSet = new DataSet("testName");
+	//		dataSet.addCondition("projects", "project_id > 10");
+	//		dataSet.addCondition("projects", "project_name <> 'project_1'");
+	//		try {
+	//			dataSet.save();
+	//			success = true;
+	//		} catch (SQLException e) {
+	//			LOGGER.fatal("", e);
+	//		} catch (ClassNotFoundException e) {
+	//			LOGGER.fatal("", e);
+	//		}
+	//		_manager.rollback();
+	//		assertTrue(success);
+	//	}
+	//	
+	//	public void testSave2()
+	//	{
+	//		LOGGER.info("DataSetTest.testSave2()");
+	//		boolean success = false;
+	//		DataSet dataSet = new DataSet("testName2");
+	//		dataSet.addCondition("projects", "project_id > 10");
+	//		dataSet.addCondition("projects", "project_name <> 'project_1'");
+	//		dataSet.addCondition("tasks", "task_id = 2");
+	//		try {
+	//			dataSet.save();
+	//			success = true;
+	//		} catch (SQLException e) {
+	//			LOGGER.fatal("", e);
+	//		} catch (ClassNotFoundException e) {
+	//			LOGGER.fatal("", e);
+	//		}
+	//		_manager.rollback();
+	//		assertTrue(success);
+	//	}
 	/**
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception
 	{
 		PropertyConfigurator.configure("log.conf"); //$NON-NLS-1$   
-		_manager = new DBManager() ; 
-		_manager.connect(); 
+		_manager = new DBManager();
+		_manager.connect();
 	}
+
 	private static final Logger LOGGER = Logger.getLogger(DataSetTest.class);
 
 }
--- salomon/platform/trunk/test/junit/src/salomon/engine/solution/SolutionManagerTest.java
+++ salomon/platform/trunk/test/junit/src/salomon/engine/solution/SolutionManagerTest.java
@@ -18,16 +18,30 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  * 
  */
+
 package salomon.engine.solution;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import junit.framework.TestCase;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
+import salomon.engine.database.queries.SQLSelect;
+
+import salomon.util.gui.Utils;
+
+import salomon.platform.IDataEngine;
+import salomon.platform.IUniqueId;
+import salomon.platform.data.dataset.IDataSet;
+import salomon.platform.exception.PlatformException;
+
 import salomon.engine.platform.ManagerEngine;
 
-public class SolutionManagerTest extends TestCase {
+public class SolutionManagerTest extends TestCase
+{
 
 	/**
 	 * 
@@ -36,71 +50,123 @@ public class SolutionManagerTest extends TestCase {
 	 */
 	private SolutionManager _solutionManager;
 
-    
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(SolutionManagerTest.class);
-    }
+	public static void main(String[] args)
+	{
+		junit.textui.TestRunner.run(SolutionManagerTest.class);
+	}
 
-    /*
-     * @see TestCase#setUp()
-     */
-    protected void setUp() throws Exception {
-        super.setUp();
-        PropertyConfigurator.configure("log.conf");
-        ManagerEngine engine = new ManagerEngine();
-        _solutionManager = (SolutionManager) engine.getSolutionManager();
-        
-    }
-    
-    /**
-     * 
-     */
-    public void testCreateSolution() {
-        LOGGER.info("SolutionManager.testCreateSolution()");
-        ISolution solution = _solutionManager.createSolution() ;
-        assertNotNull(solution);
-        
-    }
-    
-    /**
-     * 
-     */
-    public void testGetAddSolution() {
-        LOGGER.info("SolutionManager.testGetAddSolution()");
-        
-        //boolean result = false ; 
-        //ISolution solution = Solution.getInstance() ;
-        //_solutionManager.addSolution(solution) ;
-        
-        ISolution solutions[] = _solutionManager.getSolutions() ;
-        assertNotNull(solutions) ;
-        
-        /*
-        for (int i = 0; i < solutions.length; i++) {
-            if (solutions[i].equals(solution)) {
-                result = true ;
-                break ;
-            }
-        }
-        
-        assertTrue(result);
-        */
-        
-    }
+	/*
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception
+	{
+		super.setUp();
+		PropertyConfigurator.configure("log.conf");
+		ManagerEngine engine = new ManagerEngine();
+		_solutionManager = (SolutionManager) engine.getSolutionManager();
 
-    /**
-     * 
-     */
-    public void testGetSolution() {
-       LOGGER.info("SolutionManager.testGetSolution()");
-       ISolution solution = _solutionManager.createSolution() ;
-       assertTrue("TEST AWAITING FOR Solution INTERFACE", false) ;
- 
-       //ISolution gotsolution = _solutionManager.getSolution(null) ;
-       //assertNotNull(gotsolution) ;
+	}
 
-    }
-    
-    
-    private static Logger LOGGER = Logger.getLogger(SolutionManager.class);
+	/**
+	 * 
+	 */
+	public void testCreateSolution()
+	{
+		LOGGER.info("SolutionManager.testCreateSolution()");
+		ISolution solution = null;
+		try {
+			solution = _solutionManager.createSolution();
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		}
+		assertNotNull(solution);
+
+	}
+
+	/**
+	 * 
+	 */
+//	public void testGetAddSolution()
+//	{
+//		LOGGER.info("SolutionManager.testGetAddSolution()");
+//
+//		//boolean result = false ; 
+//		//ISolution solution = Solution.getInstance() ;
+//		//_solutionManager.addSolution(solution) ;
+//
+//		ISolution solutions[] = _solutionManager.getSolutions();
+//		assertNotNull(solutions);
+//
+//		/*
+//		 for (int i = 0; i < solutions.length; i++) {
+//		 if (solutions[i].equals(solution)) {
+//		 result = true ;
+//		 break ;
+//		 }
+//		 }
+//		 
+//		 assertTrue(result);
+//		 */
+//
+//	}
+
+	public void testGetDataEngine()
+	{
+		ISolution solution = null;
+		try {
+			solution = _solutionManager.getSolution(new IUniqueId() {
+				public int getId()
+				{
+					return 1;
+				}
+			});
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		}
+		assertNotNull(solution);
+		IDataEngine dataEngine = null;
+		try {
+			dataEngine = solution.getDataEngine();
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		}
+		IDataSet dataSet = null;
+		try {
+			dataSet = dataEngine.getDataSetManager().createEmpty();
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		}
+		assertNotNull(dataSet);
+		SQLSelect select = new SQLSelect();
+		select.addTable("persons");
+		ResultSet resultSet = null;
+		try {
+			resultSet = dataSet.selectData(select);
+			Utils.createResultTable(resultSet);
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		} catch (SQLException e) {
+			LOGGER.fatal("", e);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void testGetSolution()
+	{
+		LOGGER.info("SolutionManager.testGetSolution()");
+		try {
+			ISolution solution = _solutionManager.createSolution();
+		} catch (PlatformException e) {
+			LOGGER.fatal("", e);
+		}
+		assertTrue("TEST AWAITING FOR Solution INTERFACE", false);
+
+		//ISolution gotsolution = _solutionManager.getSolution(null) ;
+		//assertNotNull(gotsolution) ;
+
+	}
+
+	private static Logger LOGGER = Logger.getLogger(SolutionManager.class);
 }
