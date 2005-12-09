27b6ae88e07994c2ecfcccc9464a8c88e370423f
krzycho <krzycho@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2005-12-09 22:15:08 +0000

    Tests for conditions added.

---
+++ salomon/platform/trunk/test/junit/src/salomon/engine/platform/data/dataset/condition/ConditionTests.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2005 Salomon Team
+ *
+ * This file is part of Salomon.
+ *
+ * Salomon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Salomon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Salomon; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+package salomon.engine.platform.data.dataset.condition;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class ConditionTests
+{
+
+	public static Test suite()
+	{
+		TestSuite suite = new TestSuite(
+				"Test for salomon.engine.platform.data.dataset.condition");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(EqualsConditionTest.class);
+		suite.addTestSuite(GreaterConditionTest.class);
+		suite.addTestSuite(LowerConditionTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
