0da5d5ca038a48f46194b1801489d02ee5a62771
nico <nico@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2006-02-03 20:36:16 +0000

    [Task #65] DataSetCreator draft.

---
+++ salomon/plugins/DataSetCreator/src/pl/edu/agh/iisg/salomon/plugin/datasetcreator/DataSetCreatorPlugin.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2006 Salomon Team
+ *
+ * This file is part of Salomon.
+ *
+ * Salomon is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Salomon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Salomon; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
+package pl.edu.agh.iisg.salomon.plugin.datasetcreator;
+
+import salomon.platform.IDataEngine;
+import salomon.platform.IEnvironment;
+import salomon.plugin.IPlugin;
+import salomon.plugin.IResult;
+import salomon.plugin.IResultComponent;
+import salomon.plugin.ISettingComponent;
+import salomon.plugin.ISettings;
+
+/**
+ * @author nico
+ */
+public final class DataSetCreatorPlugin implements IPlugin
+{
+
+	/**
+	 *
+	 */
+	public IResult doJob(IDataEngine arg0, IEnvironment arg1, ISettings arg2)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *
+	 */
+	public ISettingComponent getSettingComponent()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 *
+	 */
+	public IResultComponent getResultComponent()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
