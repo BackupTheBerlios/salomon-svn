61d2d0ce2a5816cc660762dea4c78e1c67956b7b
krzycho <krzycho@365c1dc3-c9e6-0310-9dba-f6f9a6fbeba6>
2006-03-17 13:03:06 +0000

    Compilation problem fixed.

--- salomon/plugins/TestAttrSetPlugin/src/pl/capitol/tree/plugins/dataloader/components/TestAttrSetResultComponent.java
+++ salomon/plugins/TestAttrSetPlugin/src/pl/capitol/tree/plugins/dataloader/components/TestAttrSetResultComponent.java
@@ -1,20 +1,27 @@
+
 package pl.capitol.tree.plugins.dataloader.components;
 
 import java.awt.Component;
+
+import salomon.platform.IDataEngine;
+
 import salomon.plugin.IResult;
 import salomon.plugin.IResultComponent;
 
 import pl.capitol.tree.plugins.dataloader.panels.TestAttrSetResultPanel;
 import pl.capitol.tree.plugins.util.TestAttrSetResults;
 
-public class TestAttrSetResultComponent implements IResultComponent {
+public class TestAttrSetResultComponent implements IResultComponent
+{
 
-	public Component getComponent(IResult result) {
-		return new TestAttrSetResultPanel(result); 
-	}
+    public Component getComponent(IResult result, IDataEngine dataEngine)
+    {
+        return new TestAttrSetResultPanel(result);
+    }
 
-	public IResult getDefaultResult() {
-		return new TestAttrSetResults();
-	}
+    public IResult getDefaultResult()
+    {
+        return new TestAttrSetResults();
+    }
 
 }
